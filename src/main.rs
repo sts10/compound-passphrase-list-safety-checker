913261fce26d4946b54a63851def287445f7f593
sts10 <sschlinkert@gmail.com>
2018-02-27 20:49:21 -0500

    removes some bad code commented out

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use std::io::BufRead;
 use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
-
-    // println!("list {:?}", make_vec("eff-word-list.txt"));
     println!(
         "Mashed word list is {:?}",
         mash_words(make_vec("eff-word-list.txt"))
@@ -56,31 +53,3 @@ fn search(word_list: Vec<String>, mashed_words_list: Vec<String>) -> Vec<String>
     }
     return bad_words;
 }
-
-// fn search_for_sum_words(word_list: Vec<String>) -> Vec<String> {
-//     let mut sum_words: Vec<String> = [].to_vec();
-//     for word in word_list {
-//         let length: usize = word.len();
-//         let vec: Vec<&str> = word.split("").collect();
-//         // (1) Iterate  as many times as length
-//         for i in 1..length {
-//             let first_split = vec[1..i + 1].join("");
-//             for word_to_check_first_split in &word_list {
-//                 // (2) compare first split to every word in word list
-//                 if &first_split == word_to_check_first_split {
-//                     // (3) if a match, immediately compare second split to
-//                     // every word in word list
-//                     let second_split = vec[i..length + 1].join("");
-//                     for word_to_check_second_split in &word_list {
-//                         if &second_split == word_to_check_second_split {
-//                             // (4) if find a match there, add to sum_words vec
-//                             sum_words.push(word);
-//                             // and (5) move to next word
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     return sum_words;
-// }
